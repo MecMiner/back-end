5b7e856619d6ba1473a6770a05b54f87f2d62f26
MecMiner <souza.jefersoncarlos@gmail.com>
2023-08-31 20:48:18 -0300

    teste

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/', verifyToken,async (req, res) => {
     }).then((dataUsaurio) => {
         return res.json({
             erro: false,
-            dataUsuario
+            dataUsuario: dataUsuario
         })
     }).catch(() => {
         return res.json({
